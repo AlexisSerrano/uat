d14ac573cdab7460ab560483370c10a815b27e25
Alfredo Viveros\FGE <aviveros@fiscaliaveracruz.gob.mx>
2018-04-23 07:38:34 -0500

    migracion de usuarios

--- database/migrations/2018_11_10_052741_create_users_table.php
+++ database/migrations/2018_11_10_052741_create_users_table.php
@@ -15,11 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',100);
-            $table->string('email',100)->unique();
-            $table->string('password',100);
+            $table->integer('idUnidad');//->nullable();
+            $table->integer('idZona');//->nullable();
+            $table->string('username', 20);
+            $table->string('nombres', 50);
+            //$table->string('apellidos', 50);
+            $table->string('email', 100)->unique();
+            $table->string('password', 100);
+            // $table->integer('numFiscal');
+            //$table->enum('nivel', ['1', '2', '3', '4', '5'])->default('1');
+
+            //$table->foreign('idUnidad')->references('id')->on('unidad')->onDelete('cascade');
+
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
