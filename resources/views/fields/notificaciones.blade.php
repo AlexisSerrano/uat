1130e6f0a6dde9d40acebc7f45cc29ec2a012745
Alfredo Viveros\FGE <aviveros@fiscaliaveracruz.gob.mx>
2018-05-02 11:13:19 -0500

    aplicacion de validacion

--- resources/views/fields/notificaciones.blade.php
+++ resources/views/fields/notificaciones.blade.php
@@ -3,7 +3,7 @@
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('correo', 'Correo electrónico', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::email('correo', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese el correo electrónico', 'data-validation'=>'required']) !!}
+			{!! Form::email('correo', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese el correo electrónico', 'data-validation'=>'email,required']) !!}
 		</div>
 	</div>
 	<div class="col-4">
@@ -15,7 +15,7 @@
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('fax', 'Fax', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::text('fax', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese el fax']) !!}
+			{!! Form::text('fax', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese el fax', 'data-validation'=>'number']) !!}
 		</div>
 	</div>
 </div>
