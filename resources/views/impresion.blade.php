db5d40d8200cad4eabc396ff20f3cd06d1c0ba7e
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-05-08 07:15:40 -0500

    modificación oficio actas de hechos

--- resources/views/impresion.blade.php
+++ resources/views/impresion.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{asset ('css/cssfonts.css')}}">    <title>Document</title>
     <style>
+        *{
+            font-family: "NeoSans";
+        }
     @media print {
         .impre {display:none}
         }
