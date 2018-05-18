1656e6f122a927549feb319972a26906330b7b2c
ADFGE\psuarez <psuarez@fiscaliaveracruz.gob.mx>
2018-05-18 08:46:34 -0500

    tablas cola y urgente

--- resources/views/servicios/recepcion/cola-preregistro.blade.php
+++ resources/views/servicios/recepcion/cola-preregistro.blade.php
@@ -5,9 +5,9 @@
 	@section('title', 'Registros urgentes')
 @endif
 @section('content')
-<div class="container">
 	<div class="row">
 		<div class="col-12">
+			
 			{!! Form::open(['route' => 'prioridadpreregistrofiltro', 'method' => 'POST'])  !!}
 			@csrf
 			@if ($status==0)
@@ -16,21 +16,23 @@
 				<input type="hidden" name="tipo" id="tipo" value=1>
 			@endif
 				<div class="row">
-					<div class="col-4 text-right">
-						<div class="input-group">
-							<input type="text" id="folio" name="folio" class="form-control" placeholder="buscar">
-							<span class="input-group-btn">
-								<button type="submit" class="btn btn-secondary"><i class="fa fa-search" aria-hidden="true"></i></button>
-							</span>
-						</div>
+					<div class=" input-group col-4 text-right">
+							<div class="input-group-prepend">
+								<span class="input-group-text" id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i></span>
+							</div>
+							<input type="text" id="folio" name="folio" class="form-control" placeholder="buscar" aria-describedby="basic-addon1">
+						
 					</div>
 				</div>
 			{!!Form::close()!!}	
 		</div>
-		<br><br>
+		<br>
+		<br>
+		<br>
 		<div class="col-12">
-			<table class="table table-striped table-bordered table-hover">
-			 	<thead class="thead-active">
+				<div class="card">
+			<table class="table table-hover">
+			 	<thead class="thead-light">
 			    	<tr>
 			    		<th scope="col">Id</th>
 			      		<th scope="col">Folio</th>
@@ -38,7 +40,7 @@
 			      		<th scope="col">Nombre</th>
 						<th scope="col">Identificación</th>
 						<th scope="col">Razón</th>
-			      		<th scope="col">Validar</th>
+			      		<th scope="col">Ver</th>
 			    	</tr>
 			  	</thead>
 			  	<tbody>
@@ -50,7 +52,7 @@
 			      		<td>{{($registro->esEmpresa==0)?$registro->nombre.' '.$registro->primerAp.' '.$registro->segundoAp:$registro->representanteLegal}}</td>
 						<td>{{$registro->docIdentificacion}}</td>
 						<td>{{$registro->razon}}</td>
-			      		<td><a href="{{url("predenuncias/".$registro->id."/edit")}}"><i class="fa fa-check-square-o" aria-hidden="true"></i></a></td>
+			      		<td><a class="btn btn-primary" href="{{url("predenuncias/".$registro->id."/edit")}}"><i class="fa fa-check" aria-hidden="true"></i></a></td>
 			    	</tr>
 					@empty
 
