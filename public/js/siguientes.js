48d16b85e20a856695ceb31704b7e8215d1ebf0d
ADFGE\psuarez <psuarez@fiscaliaveracruz.gob.mx>
2018-05-21 14:04:47 -0500

    VALIDACIONES sin datos del trabajo para turnos

--- public/js/siguientes.js
+++ public/js/siguientes.js
@@ -3,57 +3,52 @@
 //Datos personales-direccion
         $('#Adireccion').click(function(){
         //es para empresa
-        if ($('#esEmpresa1').is(':checked') ) {
-        console.log('entra empresa')
-        validarEmpresa();
-
-        }
-        else{
-        console.log('entra persona')
-        validarPersona();  
-        }	
+            if ($('#esEmpresa1').is(':checked') ) {
+                    console.log('entra empresa')
+                    validarEmpresa();
+            }
+            else{
+                    console.log('entra persona')
+                    validarPersona();  
+            }	
         });
         function validarEmpresa(){
 
         var completo=0;
 
-
-        if ($('#nombres2').val().length == 0){
-        completo=1;
-        
-        }
-        if ($('#fechaAltaEmpresa').val().length == 0){
-        completo=1;
-        }
-        if ($('#rfc2').val().length == 0){
-        completo=1;
-        }
-        if ($('#homo2').val().length == 0){
-        completo=1;
-        }
-        if ($('#representanteLegal').val().length == 0){
-        completo=1;
-        }
+                if ($('#nombres2').val().length == 0){
+                completo=1;
+                
+                }
+                if ($('#fechaAltaEmpresa').val().length == 0){
+                completo=1;
+                }
+                if ($('#rfc2').val().length == 0){
+                completo=1;
+                }
+                if ($('#homo2').val().length == 0){
+                completo=1;
+                }
+                if ($('#representanteLegal').val().length == 0){
+                completo=1;
+                }
         else{
             
-        if (completo==0) {
+            if (completo==0) {
+                $('.nav-link').removeClass("active");
+                $('#direccion-tab').addClass("active");//Agrego la clase active al tab actual
+                $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
+                $('.tab-pane').removeClass("show");
+                $('#direccion').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
+                $('#direccion').addClass("show");
+                completo=2;
+                console.log("todo bien!")
+            }
+        }   
 
-        $('.nav-link').removeClass("active");
-        $('#direccion-tab').addClass("active");//Agrego la clase active al tab actual
-        $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
-        $('.tab-pane').removeClass("show");
-        $('#direccion').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
-        $('#direccion').addClass("show");
-        completo=2;
-        console.log("todo bien!")
+     if (completo==1) {
+            toastr.error("Complete los campos faltantes para poder avanzar");
         }
-
-        else if (completo==1) {
-
-        toastr.error("Complete los campos faltantes para poder avanzar");
-        }
-
-    }   
 }
         function validarPersona(){
 
@@ -109,10 +104,9 @@
         }
         if ($('#numDocIdentificacion').val().length == 0){
                 completoP=1;
-         }
-         
+         }  
 
-        else{
+ else{
             if (completoP==0) {
                 $('.nav-link').removeClass("active");//Quito la clase active al tab actual
                 $('#direccion-tab').addClass("active");//Agrego la clase active al tab actual
@@ -120,13 +114,11 @@
                 $('.tab-pane').removeClass("show");
                 $('#direccion').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
                 $('#direccion').addClass("show");
-                console.log("todo bien")}
-                
-       
-
-       else if (completoP==1) {
-        toastr.error("Complete los campos faltantes para poder avanzar");
+                console.log("todo bien")
+            }
         }
+ if (completoP==1) {
+        toastr.error("Complete los campos faltantes para poder avanzar");
         }
     }
 
@@ -180,10 +172,10 @@
                 }        
                 
             }
-            else if (direcciones==1) {
+        }
+     if (direcciones==1) {
                 toastr.error("Complete los campos faltantes para poder avanzar");
             }
-        }
         }
 
         $('#ANotificaciones').click(function(){
@@ -232,7 +224,6 @@
                
                 if ($('#lugarTrabajo').val().length == 0){
                 trabajo=1;
-                toastr.warning("Complete el campo de Lugar de trabajo o sin empleo antes de continuar");
                 }
                 if ($('#telefonoTrabajo').val().length == 0){
                 trabajo=1;
@@ -309,34 +300,34 @@
         function validarNotificaciones(){
         var notificaciones=0;
 
-        if ($('#idEstado3').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#idMunicipio3').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#idLocalidad3').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#idColonia3').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#cp3').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#calle3').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#numExterno3').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#correo').val().length == 0){
-        notificaciones=1;
-        }
-        if ($('#telefonoN').val().length == 0){
-        notificaciones=1;
+                if ($('#idEstado3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#idMunicipio3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#idLocalidad3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#idColonia3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#cp3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#calle3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#numExterno3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#correo').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#telefonoN').val().length == 0){
+                notificaciones=1;
 
-        }
+                }
         else{
             if (notificaciones==0) {
                 
@@ -348,10 +339,10 @@
                 $('#denunciante').addClass("show");
                 console.log('na')
             }
-            else if (notificaciones==1) {
+        }
+         if (notificaciones==1) {
                 toastr.error("Complete los campos faltantes para poder avanzar");
             }
-        }
         }
 
         $('#Adenunciado').click(function(){
@@ -361,53 +352,51 @@
         function validarNotificaciones2(){
             var notificaciones=0;
     
-            if ($('#idEstado3').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#idMunicipio3').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#idLocalidad3').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#idColonia3').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#cp3').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#calle3').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#numExterno3').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#correo').val().length == 0){
-            notificaciones=1;
-            }
-            if ($('#telefonoN').val().length == 0){
-            notificaciones=1;
-    
-            }
-            else{
-                if (notificaciones==0) {
-                    
-                    $('.nav-link').removeClass("active");//Quito la clase active al tab actual
-                    $('#denunciado-tab').addClass("active");//Agrego la clase active al tab actual
-                    $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
-                    $('.tab-pane').removeClass("show");
-                    $('#denunciado').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
-                    $('#denunciado').addClass("show");
-                    console.log('na')
+                if ($('#idEstado3').val().length == 0){
+                notificaciones=1;
                 }
-                else if (notificaciones==1) {
-                    toastr.error("Complete los campos faltantes para poder avanzar");
+                if ($('#idMunicipio3').val().length == 0){
+                notificaciones=1;
                 }
-            }
-            }
+                if ($('#idLocalidad3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#idColonia3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#cp3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#calle3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#numExterno3').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#correo').val().length == 0){
+                notificaciones=1;
+                }
+                if ($('#telefonoN').val().length == 0){
+                notificaciones=1;}
+    else{
+        if (notificaciones==0) {
+            
+            $('.nav-link').removeClass("active");//Quito la clase active al tab actual
+            $('#denunciado-tab').addClass("active");//Agrego la clase active al tab actual
+            $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
+            $('.tab-pane').removeClass("show");
+            $('#denunciado').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
+            $('#denunciado').addClass("show");
+            console.log('na')
+        }
+    }
+    if (notificaciones==1) {
+            toastr.error("Complete los campos faltantes para poder avanzar");
+        }
+    }
 
-        function validarNotDenunciado(){
-        var notificaciones=0;
+    function validarNotDenunciado(){
+    var notificaciones=0;
 
         if ($('#idEstado3').val().length == 0){
             notificaciones=1;
@@ -434,10 +423,8 @@
         notificaciones=1;
         }
         if ($('#telefonoN').val().length == 0){
-        notificaciones=1;
-
-        }
-        else{
+        notificaciones=1;}
+    else{
             if (notificaciones==0) {
                 
                 $('.nav-link').removeClass("active");//Quito la clase active al tab actual
@@ -448,18 +435,18 @@
                 $('#denunciado').addClass("show");
                 console.log('nan')
             }
-            else if (notificaciones==1) {
+        }
+            if (notificaciones==1) {
                 toastr.error("Complete los campos faltantes para poder avanzar");
             }
-        }
         }
 
         $('#guardarDenunciante').click(function(){
         validarDatosDenunciante();
         });
 
-        function validarDatosDenunciante(){
-        var datosDenunciante=0;
+    function validarDatosDenunciante(){
+    var datosDenunciante=0;
 
         if ($('#reguardarIdentidad').val().length == 0){
             datosDenunciante=1;
@@ -475,54 +462,54 @@
         }
     }
 
-            $('#Aautoridad').click(function(){
-            validarTrabajo3();
-            });
-            function validarTrabajo3(){
-                var trabajo=0;
-                if ($('#lugarTrabajo').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#telefonoTrabajo').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#idEstado2').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#idMunicipio2').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#idLocalidad2').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#idColonia2').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#cp2').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#calle2').val().length == 0){
-                trabajo=1;
-                }
-                if ($('#numExterno2').val().length == 0){
-                trabajo=1;
-                }
-                else{
-                    if (trabajo==0) {
-                        
-                        $('.nav-link').removeClass("active");//Quito la clase active al tab actual
-                        $('#autoridad-tab').addClass("active");//Agrego la clase active al tab actual
-                        $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
-                        $('.tab-pane').removeClass("show");
-                        $('#autoridad').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
-                        $('#autoridad').addClass("show");
-                        console.log('a notificaciones')
-                    }
-                    else if (trabajo==1) {
-                        toastr.error("Complete los campos faltantes para poder avanzar");
-                    }
-                }
-                }
+    $('#Aautoridad').click(function(){
+    validarTrabajo3();
+    });
+
+    function validarTrabajo3(){
+        var trabajo=0;
+            if ($('#lugarTrabajo').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#telefonoTrabajo').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#idEstado2').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#idMunicipio2').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#idLocalidad2').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#idColonia2').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#cp2').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#calle2').val().length == 0){
+            trabajo=1;
+            }
+            if ($('#numExterno2').val().length == 0){
+            trabajo=1;
+            }
+     else{
+            if (trabajo==0) {
+                    
+                    $('.nav-link').removeClass("active");//Quito la clase active al tab actual
+                    $('#autoridad-tab').addClass("active");//Agrego la clase active al tab actual
+                    $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
+                    $('.tab-pane').removeClass("show");
+                    $('#autoridad').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
+                    $('#autoridad').addClass("show");
+                    console.log('a notificaciones')
+             }
+    }
+     if (trabajo==1) {
+                    toastr.error("Complete los campos faltantes para poder avanzar");}
+     }
 
         //ABOGADO
             $('#Atrabajo2').click(function(){
@@ -534,22 +521,21 @@
                 if ($('.persona').val().length == 0){
                 completoP=1;
                 }
-                else{
-                    if (completoP==0) {
+         else{
+                if (completoP==0) {
+                    $('.nav-link').removeClass("active");//Quito la clase active al tab actual
+                    $('#trabajo-tab').addClass("active");//Agrego la clase active al tab actual
+                    $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
+                    $('.tab-pane').removeClass("show");
+                    $('#trabajo').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
+                    $('#trabajo').addClass("show");
+                    console.log("todo bien")}
                         
-                        $('.nav-link').removeClass("active");//Quito la clase active al tab actual
-                        $('#trabajo-tab').addClass("active");//Agrego la clase active al tab actual
-                        $('.tab-pane').removeClass("active");//quito las clases del div contenedor personas para ocultar la info
-                        $('.tab-pane').removeClass("show");
-                        $('#trabajo').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
-                        $('#trabajo').addClass("show");
-                        console.log("todo bien")}
-                        
-                        else if (completoP==1) {
-                            toastr.error("Complete los campos faltantes para poder avanzar");
-                        }
-                    }
-                }
+             }
+             if (completoP==1) {
+                toastr.error("Complete los campos faltantes para poder avanzar");
+            }
+    }
 
             $('#aAbogado').click(function(){
             validarTrabajo2();
@@ -595,10 +581,10 @@
                         
                         console.log('a notificaciones')
                     }
-                    else if (trabajo==1) {
+                }
+                if (trabajo==1) {
                         toastr.error("Complete los campos faltantes para poder avanzar");
                     }
-                }
             }
 
             $('#aDelito').click(function(){
@@ -622,10 +608,10 @@
                     $('#dirdelito').addClass("active");//agrego las clases del div contenedor direcciones para mostrar la info
                     $('#dirdelito').addClass("show"); 
                 }
-                else if (valDelito==1) {
+            }
+             if (valDelito==1) {
                     toastr.error("Complete los campos faltantes para poder avanzar");
                 }
-            }
 
         }
 
