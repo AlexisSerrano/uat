901fe44d8b0b8024b35c9900dba04f6bc9e008aa
ADFGE\psuarez <psuarez@fiscaliaveracruz.gob.mx>
2018-05-22 14:41:55 -0500

    numero interno opcional front

--- resources/views/fields/actas.blade.php
+++ resources/views/fields/actas.blade.php
@@ -108,7 +108,7 @@
 <div class="col-2">
     <div class="form-group">
         {!! Form::label('numInterno2', 'Número interior', ['class' => 'col-form-label-sm']) !!}
-        {!! Form::text('numInterno2', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese el n. Interior', 'data-validation'=>'required']) !!}
+        {!! Form::text('numInterno2', null, ['class' => 'form-control form-control-sm', 'placeholder' => 'Ingrese el n. Interior', 'data-validation'=>'custom','data-validation-optional'=>'true']) !!}
     </div>
 </div>
 
