c6ac99282ebeb37c267f7d7f9421e0bd4fa882e1
ADFGE\avilaf <avilaf@fiscaliaveracruz.gob.mx>
2018-05-30 11:33:36 -0500

    archivos para crear acta circunstanciada

---
+++ resources/views/forms/acta-circunstanciada.blade.php
@@ -0,0 +1,35 @@
+@extends('template.form')
+
+@section('title', 'Acta circunstanciada')
+@section('content')
+@include('fields.errores')
+
+{!! Form::open(['route' => 'addactaCirc', 'method' => 'post'])  !!}
+{{-- {!! Form::open(['route' => 'addactaCirc', 'method' => 'POST'])  !!} --}}
+	<br>
+<div class="row no-gutters">
+    <div class="col-12">
+        <div class="boxtwo">
+            <div class="row" id="actas">
+                {{-- DATOS DE TODO TIPO DE ACTAS --}}
+                @isset($acta)
+                    {{-- @include('fields.datos-actaCircunst') --}}
+                @else
+                    @include('fields.datos-actaCircunst')
+                @endisset
+            </div>
+           
+                <div class="col text-right">
+                    {!!Form::submit('Guardar',array('class' => 'btn btn-primary','id'=>'guardarActa'))!!} 
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+	{!! Form::close() !!}
+	
+@endsection
+{{-- @push('scripts')
+<script src="{{ asset('js/actas.js') }}"></script>
+@endpush --}}
