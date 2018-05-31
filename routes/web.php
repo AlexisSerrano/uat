154f36e83f84b6d6dfee3a397f59abcd5c5675be
ADFGE\psuarez <psuarez@fiscaliaveracruz.gob.mx>
2018-05-31 11:02:39 -0500

    ruta vehiculos

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/pruebasconsulta', function(){
         
  /* --------Pruebas vehiculos----------- */
 
- Route::get('/vehiculos-pruebas','VehiculoController@showform');
+ Route::get('/vehiculos-pruebas','VehiculoController@showform')->name('vehiculo.carpeta');
 
 
 /**************************************************************/
