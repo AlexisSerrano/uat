d2d7367c91c2f2cbf1f9cd1395407b9aa20b110d
Alfredo Viveros\FGE <aviveros@fiscaliaveracruz.gob.mx>
2018-06-06 12:11:51 -0500

    resumen de carpeta: se hiso la barra de navegacion de la derecha de forma dinamica(que solo aparesca lo que se tiene en la carpeta)

---
+++ resources/views/fields/resumen-carpeta/botonesResumen.blade.php
@@ -0,0 +1,112 @@
+@php
+$barra = getNavCaso();
+@endphp
+
+@section('css')
+    <style>
+        .active1{
+            background: #8e8e8e;
+            color: #000000;
+        }
+    </style>
+@endsection
+
+<div class="col-2">
+    <div class="card">
+        <div class="card-header"><h6>Elementos de la carpeta</h1></div>
+            <div class=" panel panel-default">
+                <div class="panel-body">
+                    <table class="table">
+                        <tbody>
+                            {{-- @forelse($oficios as $oficio) --}}
+                            @if($barra['cdenunciante']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-denunciante') ? 'active1' : '' }} btn btn-default denunciante" href="{{route('resumen.denunciante')}}">
+                                    <span class="badge badge-info right">{{$barra['cdenunciante']}}</span> Denunciante 
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['cdenunciado']>0)
+                            <tr>
+                                <td style="padding:0; text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-denunciado') ? 'active1' : '' }} btn btn-default denunciado" href="{{route('resumen.denunciado')}}">
+                                    <span class="badge badge-info left">{{$barra['cdenunciado']}}</span> Denunciado </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['cabogado']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-abogado') ? 'active1' : '' }} btn btn-default abogado" href="{{route('resumen.abogado')}}">
+                                    <span class="badge badge-info right">{{$barra['cabogado']}}</span> Abogado </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['cautoridad']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-autoridad') ? 'active1' : '' }} btn btn-default autoridad" href="{{route('resumen.autoridad')}}">
+                                        <span class="badge badge-info right">{{$barra['cautoridad']}}</span> Autoridad 
+                                    </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['cdelitos']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-delito') ? 'active1' : '' }} btn btn-default delitos" href="{{route('resumen.delito')}}">
+                                        <span class="badge badge-info right">{{$barra['cdelitos']}}</span> Delitos
+                                    </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['cacusaciones']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-acusaciones') ? 'active1' : '' }} btn btn-default acusaciones" href="{{route('resumen.acusaciones')}}">
+                                        <span class="badge badge-info right">{{$barra['cacusaciones']}}</span> Acusaciones
+                                    </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['cdefensa']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-defensa') ? 'active1' : '' }} btn btn-default defensa" href="{{route('resumen.defensa')}}">
+                                        <span class="badge badge-info right">{{$barra['cdefensa']}}</span> Defensa
+                                    </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['cvehiculos']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:left;" class="{{ Request::is( 'resumen-vehiculo') ? 'active1' : '' }} btn btn-default vehiculos" href="{{route('resumen.observaciones')}}">
+                                        <span class="badge badge-info right">0{{$barra['cdefensa']}}</span> Vehiculos
+                                    </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            @if($barra['chechos']>0)
+                            <tr>
+                                <td style="padding:0;text-align:left;">
+                                    <a style="width:100%;text-align:center;" class="{{ Request::is( 'resumen-observaciones') ? 'active1' : '' }} btn btn-default observaciones" href="{{route('resumen.observaciones')}}">
+                                        Observaciones
+                                    </a>
+                                </td>
+                            </tr>
+                            @endisset
+                            {{-- @empty --}}
+
+                            {{-- @endforelse --}}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+            
+            
--- resources/views/fields/resumen-carpeta/resumen-carpeta.blade.php
+++ resources/views/fields/resumen-carpeta/resumen-carpeta.blade.php
@@ -14,64 +14,11 @@
             </div>
         </div>
         
-        <div class="col-2">
-            <div class="card">
-                <div class="card-header"><h6>Elementos de la carpeta</h1></div>
-                        <div>
-                            <div class=" panel panel-default">
-                                <div class="panel-body">
-                                    <table class="table">
-                                        <tbody>
-                                            {{-- @forelse($oficios as $oficio) --}}
-                                            <tr>
-                                                <td style="padding:0; text-align:left;">
-                                                    <a style="width:100%;text-align:left;" class="btn btn-default denunciado" href="{{route('resumen.denunciado')}}">
-                                                    <span class="badge badge-info left">2</span> Denunciado </a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding:0;text-align:left;">
-                                                        <a style="width:100%;text-align:left;" class="btn btn-default denunciante" href="{{route('resumen.denunciante')}}">
-                                                    <span class="badge badge-info right">1</span> Denunciante </td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding:0;text-align:left;">
-                                                        <a style="width:100%;text-align:left;" class="btn btn-default abogado" href="{{route('resumen.abogado')}}">
-                                                            <span class="badge badge-info right">0</span> Abogado </a></td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding:0;text-align:left;">
-                                                        <a style="width:100%;text-align:left;" class="btn btn-default autoridad" href="{{route('resumen.autoridad')}}"><span class="badge badge-info right">0</span> Autoridad </a></td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding:0;text-align:left;">
-                                                        <a style="width:100%;text-align:left;" class="btn btn-default delitos" href="{{route('resumen.delito')}}"><span class="badge badge-info right">0</span> Delitos</a></td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding:0;text-align:left;">
-                                                        <a style="width:100%;text-align:left;" class="btn btn-default acusaciones" href="{{route('resumen.acusaciones')}}"><span class="badge badge-info right">0</span> Acusaciones</a></td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding:0;text-align:left;">
-                                                        <a style="width:100%;text-align:left;" class="btn btn-default defensa" href="{{route('resumen.defensa')}}"><span class="badge badge-info right">0</span> Defensa</a></td>
-                                            </tr>
-                                            <tr>
-                                                <td style="padding:0;text-align:left;">
-                                                        <a style="width:100%;text-align:left;" class="btn btn-default observaciones" href="{{route('resumen.observaciones')}}"><span class="badge badge-info right">0</span> Observaciones</a></td>
-                                            </tr>
-                                            {{-- @empty --}}
-                                            
-                                            {{-- @endforelse --}}
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-         </div>
-           
-            
+        @include('fields.resumen-carpeta.botonesResumen')
+    </div>
+</div>
+        
+        
             
 @endsection
 @push('scripts')
