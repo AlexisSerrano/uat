ea4128be2b762d40833e110a4131b4d2c03f7438
ingjuliovasquez <ingjuliovasquez@gmail.com>
2018-06-07 12:35:18 -0500

    nueva modificacion de marca cTALOGO

--- database/migrations/2018_05_22_130513_create_cat_marca_table.php
+++ database/migrations/2018_05_22_130513_create_cat_marca_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCatMarcasTable extends Migration
+class CreateCatMarcaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCatMarcasTable extends Migration
      */
     public function up()
     {
-        Schema::create('cat_marcas', function (Blueprint $table) {
+        Schema::create('cat_marca', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre', 50)->unique();
 
@@ -29,6 +29,6 @@ class CreateCatMarcasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cat_marcas');
+        Schema::dropIfExists('cat_marca');
     }
 }
