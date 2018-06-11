7a54dbf76e3cff6a6188f21d187e190cfa3dbddd
ingjuliovasquez <ingjuliovasquez@gmail.com>
2018-06-11 13:04:47 -0500

    nuevos cambios

---
+++ app/Http/Controllers/LibroOficioController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LibroOficioController extends Controller
+{
+    //
+
+
+    public function oficioindex()
+    {   
+
+    return view('forms.periciales');
+}
+
+}
+
