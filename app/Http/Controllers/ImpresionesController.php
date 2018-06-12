d2fa13df624a94288a5e5f8c2c5239849bb3a130
ADFGE\psuarez <psuarez@fiscaliaveracruz.gob.mx>
2018-06-12 09:34:15 -0500

    acuerdo fiscal distrito

--- app/Http/Controllers/ImpresionesController.php
+++ app/Http/Controllers/ImpresionesController.php
@@ -33,14 +33,42 @@ class ImpresionesController extends Controller
             ->orderBy('descripcion', 'ASC')
             ->pluck('descripcion', 'id');
 
-             //(dd($unidad);
+            $denunciado['']='Seleccione a la persona denunciada';
+            $denunciado2= DB::table('variables_persona')
+            ->join('persona', 'variables_persona.idPersona', '=', 'persona.id')
+            ->join('extra_denunciado', 'variables_persona.id', '=', 'extra_denunciado.idVariablesPersona')
+            ->where('variables_persona.idCarpeta',$idCarpeta)
+            ->select('persona.nombres','persona.primerAp','persona.segundoAp', 'persona.id')
+            ->get();
+
+            foreach($denunciado2 as $denunciado){
+                $denunciado2[$denunciado->nombres.'-'.$denunciado->primerAp.'-'.$denunciado->segundoAp] = $denunciado->nombres.' '.$denunciado->primerAp.' '.$denunciado->segundoAp;
+            }
+
+            //dd($denunciado);
+
+            $victimas[''] = 'Seleccione una víctima/ofendido';
+            $victimas2 = DB::table('variables_persona')
+            ->join('persona', 'variables_persona.idPersona', '=', 'persona.id')
+            ->join('extra_denunciante', 'variables_persona.id', '=', 'extra_denunciante.idVariablesPersona')
+            ->where('variables_persona.idCarpeta',$idCarpeta)
+            ->select('persona.nombres','persona.primerAp','persona.segundoAp', 'persona.id')
+            ->get();
+
+            foreach($victimas2 as $victima){
+                $victimas[$victima->nombres.'-'.$victima->primerAp.'-'.$victima->segundoAp] = $victima->nombres.' '.$victima->primerAp.' '.$victima->segundoAp;
+            }
+
             
             return view('fields.oficioDistrito')
             ->with('carpeta',$carpeta)
-            ->with('unidad',$unidad);
+            ->with('unidad',$unidad)
+            ->with('denunciado',$denunciado)
+            ->with('victimas', $victimas);
         }
 
         public function getFiscal(Request $request, $id){
+
             // if($request->ajax()){
             if($request){
                 // dd('asdasd');
@@ -58,7 +86,7 @@ class ImpresionesController extends Controller
         public function storeDistrito(Request $request){
            
             
-            return view('fields.oficioDistrito')
+            return view('fields.oficioDistrito');
            
         }
 }
