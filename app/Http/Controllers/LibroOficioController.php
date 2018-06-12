d1a8f84baea8e732d286486c589373eef4694677
ingjuliovasquez <ingjuliovasquez@gmail.com>
2018-06-12 14:47:25 -0500

    AVANCE DE .LIBRO DE OFICIOS PART1 VERSION 3.8

--- app/Http/Controllers/LibroOficioController.php
+++ app/Http/Controllers/LibroOficioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ActasHechos;
 use Jenssegers\Date\Date;
+use DB;
 
 class LibroOficioController extends Controller
 {
@@ -12,19 +13,40 @@ class LibroOficioController extends Controller
 
 
     public function IndexOfi()
-    {   
- 
+    {    
         $actas = ActasHechos::orderBy('id','desc')->paginate('15');
         $year = Date::now()->format('Y');
          return view('forms.libOficios')
          -> with("actas", $actas)
-         -> with("year", $year);
-
-
-        
+         -> with("year", $year);  
     }
 
 
+    public function filtroActas(Request $request){
+        if($request->input("filtro")!=''){
+            if($request->input("filtro")){
+                $filtro = $request->input("filtro");
+                $request->session()->flash('filtro', $filtro);
+            }
+            else{
+                $filtro = $request->session()->get('filtro');
+                $request->session()->flash('filtro', $filtro);
+    
+            }
+            $year = Date::now()->format('Y');
+            $actas = ActasHechos::
+            where(DB::raw("CONCAT(nombre,' ',primer_ap,' ',segundo_ap)"), 'LIKE', '%' . $filtro . '%')
+            ->orwhere('folio','like','%'.$filtro.'%')
+            ->paginate('15');
+            return view('forms.libOficios')
+            -> with("actas", $actas)
+            -> with("year", $year);  
+        }
+
+        else{
+            return redirect('lista-oficios');
+        }
+    }
 
 }
 
