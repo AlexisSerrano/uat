971f4610becae5fe4ab936a57dac80cca4a1ba68
ADFGE\psuarez <psuarez@fiscaliaveracruz.gob.mx>
2018-06-19 14:47:50 -0500

    testing

--- resources/views/fields/lugar-hechos.blade.php
+++ resources/views/fields/lugar-hechos.blade.php
@@ -2,37 +2,37 @@
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('entreCalle', 'Entre calle', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::text('entreCalle', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese una calle perpendicular','data-validation'=> 'required']) !!}
+			{!! Form::text('entreCalle', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese una calle perpendicular','data-validation'=>'custom','data-validation-optional'=>'true']) !!}
 		</div>
 	</div>
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('yCalle', 'Y calle', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::text('yCalle', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese otra calle perpendicular','data-validation'=> 'required']) !!}
+			{!! Form::text('yCalle', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese otra calle perpendicular','data-validation'=>'custom','data-validation-optional'=>'true']) !!}
 		</div>
 	</div>
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('calleTrasera', 'Calle trasera', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::text('calleTrasera', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese la calle trasera','data-validation'=> 'required']) !!}
+			{!! Form::text('calleTrasera', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese la calle trasera','data-validation'=>'custom','data-validation-optional'=>'true']) !!}
 		</div>
 	</div>
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('idZona', 'Zona de ubicación', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::select('idZona', $zonas, null, ['class' => ' lugarH form-control form-control-sm', 'placeholder' => 'Seleccione una zona de ubicación','data-validation'=> 'required']) !!}
+			{!! Form::select('idZona', $zonas, null, ['class' => ' lugarH form-control form-control-sm', 'placeholder' => 'Seleccione una zona de ubicación','data-validation'=>'custom','data-validation-optional'=>'true']) !!}
 		</div>
 	</div>
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('idLugar', 'Lugar', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::select('idLugar', $lugares, null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Seleccione un lugar','data-validation'=> 'required']) !!}
+			{!! Form::select('idLugar', $lugares, null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Seleccione un lugar','data-validation'=>'custom','data-validation-optional'=>'true']) !!}
 		</div>
 	</div>
 	<div class="col-4">
 		<div class="form-group">
 			{!! Form::label('puntoReferencia', 'Punto de referencia', ['class' => 'col-form-label-sm']) !!}
-			{!! Form::text('puntoReferencia', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese un punto de referencia', 'data-validation'=> 'required']) !!}
+			{!! Form::text('puntoReferencia', null, ['class' => 'lugarH form-control form-control-sm', 'placeholder' => 'Ingrese un punto de referencia', 'data-validation'=>'custom','data-validation-optional'=>'true']) !!}
 		</div>
 	</div>
 </div>
