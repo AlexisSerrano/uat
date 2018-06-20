097be27dafb5fcd69b51bdf90bfb0eb9faaf9a48
Alfredo Viveros\FGE <aviveros@fiscaliaveracruz.gob.mx>
2018-06-20 13:44:15 -0500

    acceso a carpeta para reanudar carpeta en reserva

--- app/Http/Controllers/ResumenCarpetaController.php
+++ app/Http/Controllers/ResumenCarpetaController.php
@@ -14,10 +14,18 @@ class ResumenCarpetaController extends Controller
     public function showForm($id){   
         session(['carpeta' => $id]);
         $numcarpeta=Carpeta::where('id',$id)->first();
+        $estado=$numcarpeta->idEstadoCarpeta;
         $numcarpeta=$numcarpeta->numCarpeta;
         // dd($numcarpeta);
-        session(['terminada' => $numcarpeta]);
-        return redirect(route('carpeta.detalle'));
+        // dd($numcarpeta);
+        if (is_null($estado)) {
+            session(['numCarpeta' => $numcarpeta]);
+            return redirect(route('new.denunciante'));
+        } else {
+            session(['terminada' => $numcarpeta]);
+            return redirect(route('carpeta.detalle'));
+        }
+        
     }
 
     public function showResumen(){
