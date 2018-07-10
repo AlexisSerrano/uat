926b87bef72974597d3e6407ea24d4b16e98f1e3
Alfredo Viveros\FGE <aviveros@fiscaliaveracruz.gob.mx>
2018-07-10 09:27:54 -0500

    correcciones en vistas

--- resources/views/servicios/preregistro/create.blade.php
+++ resources/views/servicios/preregistro/create.blade.php
@@ -52,9 +52,7 @@
 
 <br>
 	<p class="lead" align="center">
-
-				PRE-REGISTRO
-
+		PRE-REGISTRO
 	</p>
 	<div id="pantalla" class="oculto" >
 		<div style="font-size: 30px;text-align: center;margin-top:300px; display:block;margin-bottom: auto;">
@@ -75,7 +73,7 @@
 		<div id="collapsePersonales1">
 			<div class="boxtwo">
 				<div class="col">
-				@include('servicios.preregistro.fields.datos-personales')
+					@include('servicios.preregistro.fields.datos-personales')
 				</div>
 			</div>
 		</div>
@@ -83,7 +81,7 @@
 		<div id="collapsePersonales2">
 			<div class="boxtwo">
 				<div class="col">
-				@include('servicios.preregistro.fields.datos-empresa')
+					@include('servicios.preregistro.fields.datos-empresa')
 				</div>
 			</div>
 		</div>
@@ -103,42 +101,23 @@
 				</div>
 			</div>
 			<div class="boxtwo">
-					<div class="row">
-						
-						<div class="col">   
-							<div class="text-center">
-								<br>
-									<a href="http://fiscaliaveracruz.gob.mx/" title="" class="btn btn-primary">Cancelar</a>
-								
-									{!!Form::submit('Guardar',array('class' => 'btn btn-primary ', 'id'=>'cargando'))!!}
-									{{-- {!!Form::submit('Prueba',array('class' => 'btn btn-primary ', 'id'=>'prueba'))!!} --}}
-									{{-- <button id="prueba" type="button" class="btn btn-primary">prueba</button> --}}
-								<br>
+				<div class="row">
+					
+					<div class="col">   
+						<div class="text-center">
+							<br>
+								<a href="http://fiscaliaveracruz.gob.mx/" title="" class="btn btn-primary">Cancelar</a>
 							
-							</div>
+								{!!Form::submit('Guardar',array('class' => 'btn btn-primary ', 'id'=>'cargando'))!!}
+								{{-- {!!Form::submit('Prueba',array('class' => 'btn btn-primary ', 'id'=>'prueba'))!!} --}}
+								{{-- <button id="prueba" type="button" class="btn btn-primary">prueba</button> --}}
+							<br>
+						
 						</div>
 					</div>
 				</div>
+			</div>
 		</div>
-		{{-- <div id="cargando" style="display:none;">
-			<div class="text-center">
-				<strong  style="color:#f5f5f5;">Iniciando sesión<strong>
-			</div>
-			<div class="sk-circle">
-				<div class="sk-circle1 sk-child"></div>
-				<div class="sk-circle2 sk-child"></div>
-				<div class="sk-circle3 sk-child"></div>
-				<div class="sk-circle4 sk-child"></div>
-				<div class="sk-circle5 sk-child"></div>
-				<div class="sk-circle6 sk-child"></div>
-				<div class="sk-circle7 sk-child"></div>
-				<div class="sk-circle8 sk-child"></div>
-				<div class="sk-circle9 sk-child"></div>
-				<div class="sk-circle10 sk-child"></div>
-				<div class="sk-circle11 sk-child"></div>
-				<div class="sk-circle12 sk-child"></div>
-			</div>
-		</div> --}}
 	</div>
 		
 
