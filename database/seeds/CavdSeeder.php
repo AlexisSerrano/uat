74dddfa75ed625b1c9a24e0f8730e394f9a160dd
Felipe <wixard43@gmail.com>
2018-07-11 14:31:53 -0500

    Detalles arreglados para migrar a PostgreSql

--- database/seeds/CavdSeeder.php
+++ database/seeds/CavdSeeder.php
@@ -16,7 +16,7 @@ class CavdSeeder extends Seeder
             de la Fiscalía General del Estado
             ','direccion' => 'Circuito Rafael Guizar y Valencia núm. 707 
             Colonia Reserva Territorial Xalapa, Veracruz.
-            ', 'estatus' => '','nombreDirector' => 'Lic. Zoila Aradillas Guzmán '],
+            ', 'estatus' => 1,'nombreDirector' => 'Lic. Zoila Aradillas Guzmán '],
 
             ]);
     }
