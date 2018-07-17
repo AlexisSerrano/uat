0ce1f55bea28bb945775927a578f6bcb422c1f0e
ingjuliovasquez <ingjuliovasquez@gmail.com>
2018-07-17 15:07:53 -0500

    nuevos actas

--- app/Http/Controllers/LibroOficioController.php
+++ app/Http/Controllers/LibroOficioController.php
@@ -13,10 +13,34 @@ class LibroOficioController extends Controller
 
 
     public function IndexOfi()
-    {    
-        $actas = ActasHechos::orderBy('id','desc')->paginate('15');
+    {   
+
+        
+        
+        $vfisica = DB::table('uat.actas_hechos as actas') 
+        ->Join('componentes.variables_persona_fisica as pf', 'actas.varPersona', '=', 'pf.id') 
+        ->Join('componentes.persona_fisica as PER', 'pf.idPersona', '=', 'PER.id') 
+        ->select('actas.folio', 'actas.fiscal', 'actas.tipoActa' ,'actas.varPersona', 'pf.id' ,'pf.idPersona', 'PER.nombres', 'PER.primerAp', 'PER.segundoAp');
+        
+              
+ 
+         
+        $vmoral = DB::table('uat.actas_hechos as actas') 
+        ->Join('componentes.variables_persona_moral as pf', 'actas.varPersona', '=', 'pf.id') 
+        ->select('actas.folio', 'actas.fiscal', 'actas.tipoActa', 'actas.varPersona', 'pf.id' ,'pf.idPersona', 'pf.nombreRep', 'pf.primerApRep', 'pf.segundoApRep')   
+        ->union($vfisica)
+        
+        ->get();
+
+       
+        dd($vmoral);
+
+
+
+        // $actas = ActasHechos::orderBy('id','desc')->paginate('15');  ->where('db2.id', 5)
         $year = Date::now()->format('Y');
          return view('tables.libOficios')
+         -> with("variable", $variable)
          -> with("actas", $actas)
          -> with("year", $year);  
     }
@@ -37,6 +61,9 @@ class LibroOficioController extends Controller
             $actas = ActasHechos::
             where(DB::raw("CONCAT(nombre,' ',primer_ap,' ',segundo_ap)"), 'LIKE', '%' . $filtro . '%')
             ->orwhere('folio','like','%'.$filtro.'%')
+
+
+
             ->paginate('15');
             return view('tables.libOficios')
             -> with("actas", $actas)
@@ -44,7 +71,7 @@ class LibroOficioController extends Controller
         }
 
         else{
-            return redirect('lista-oficios');
+            return redirect('lista-ficios');
         }
     }
 
