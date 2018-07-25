d10b0b7bbb2e6d256e21ee3fc435ed54d6cca692
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-07-25 14:15:29 -0500

    trabajando en edits

---
+++ resources/views/forms/editsPersonas.blade.php
@@ -0,0 +1,14 @@
+@extends('template.form')
+
+@section('title', $editar)
+
+@section('content')
+<div id="page-content-wrapper" >
+@include('fields.buttons-navegacion')
+	<div class="col-md-12 margen">
+		<div id="app" style="background-color:#fff">
+				<registro :sistema="'uat'" tipo="{{$denunciante}}" usuario="{{Auth::user()->id}}" carpeta="{{session('numCarpeta')}}" idCarpeta="{{session('carpeta')}}" idVarPersona="{{$idVarPersona}}"></registro>
+		</div>
+	</div>
+</div>
+@endsection
