fd2aede5f8620b260306116b56c24b2a75598b68
ADFGE\psuarez <psuarez@fiscaliaveracruz.gob.mx>
2018-07-31 14:21:13 -0500

    DISEÑO/resumen delito

--- resources/views/fields/resumen-carpeta/resumen-carpeta.blade.php
+++ resources/views/fields/resumen-carpeta/resumen-carpeta.blade.php
@@ -4,12 +4,12 @@
     @include('fields.errores')
 <div class="col-12">
     <div class="row">
-        <div class="col-10">
+        {{-- <div class="col-10"> --}}
         {{-- <div class="col-10 card"> --}}
                 {{-- @include('fields.resumen-carpeta.datos-carpeta') --}}
                 @yield('detalle') 
         {{-- </div> --}}
-    </div>
+    {{-- </div> --}}
         <div class="col-2">
         
         @include('fields.resumen-carpeta.botonesResumen')
